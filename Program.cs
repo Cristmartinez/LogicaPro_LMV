640c1104a7e903554a4309a98b92e543bdb12577
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-01 21:26:41 -0500

    Actualizacion ejercicio 9

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ingrese el primer número:");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer número:");
+            int numero3 = int.Parse(Console.ReadLine());
+
+            // Determinar el mayor de los tres números
+            int mayor = Math.Max(numero1, Math.Max(numero2, numero3));
+
+            Console.WriteLine($"El mayor número ingresado es: {mayor}");
         }
     }
+    
 }
