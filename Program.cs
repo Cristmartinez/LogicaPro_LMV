7c6e3c25e1d7ce30092615b02f7338c75e5c3fa0
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-01 21:18:25 -0500

    Ejercicio 7

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ingrese un número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Verificar si el número es par o impar
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"El número {numero} es par.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {numero} es impar.");
+            }
         }
     }
 }
