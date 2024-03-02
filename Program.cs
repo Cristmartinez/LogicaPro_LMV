579f9b62d8d05e91cfc5463873f24f7e8272fa71
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-01 21:12:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicaPro_LMV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
