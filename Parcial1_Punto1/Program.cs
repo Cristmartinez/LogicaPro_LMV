8b21f9d5c48c1ab2b6c9f21314893dc7a9fd11c8
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-08 20:27:35 -0500

    Declaracion de variables

---
+++ Parcial1_Punto1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Parcial1_Punto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Un vendedor recibe un sueldo base de $1.000.000 y reciben una comision del 10% sobre el valor total de cada  venta que realiza
+
+            //Declaramos variables  
+
+            decimal Sueldobase = 1000000;
+
+            //Ventas Realizadas 
+            decimal ventas;
+            //las variables para los calculos
+            decimal comisiontotal;
+            decimal totalMes = Sueldobase;
+            decimal mayorComision;
+            decimal promedioComisiones;
+
+
+
+            
+        }
+    }
+}
