78fc9358a80f5f8047ffa83b8ceb557c044d7bf0
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-17 14:34:36 -0500

    Ejercicio13 While

---
+++ PrimerAlgoritmo_While/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PrimerAlgoritmo_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese  un número negativo, el programa debe finalizar y mostrar en consola la cantidad total de números positivos que ingresó hasta antes de ingresarse el número negativo.
+
+            //Valores de entrada o Variables 
+
+
+            int num, contador = 0;
+            bool validador = true;
+
+            // Creacion del Ciclo
+
+            while (validador == true ) 
+            {
+                Console.Write("Ingrese un numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num < 0 )
+                {
+                   validador = false;
+                }
+                else 
+                { 
+                   contador = contador + 1;
+
+
+                }
+            }
+            Console.WriteLine(String.Format("El total de numeros positivos ingresados es de: {0}" , contador));
+        }
+    }
+}
