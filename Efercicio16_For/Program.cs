398e9475bd11fd7b30b3ad636e5cd42dcd263746
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-20 19:52:39 -0500

    Sincronizacion con git

---
+++ Efercicio16_For/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Efercicio16_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // elabora un algoritmo que imprima los numeros del 1 al 100, que clacule la sima de toddos los numenros pares por un lado, y por otro, la de todos los impares 
+
+            // declaracion de variables 
+
+            int acomuladorPar = 0, acomuladorImpar = 0;
+
+            for ( int i = 1 ; i <= 100; i++)
+            {
+                // mostrar los 100 primeros numeros
+                Console.WriteLine(i);
+
+                //Identificar para acomular para par o impar
+                if (i % 2 == 0) acomuladorPar += i;
+                else acomuladorImpar += i;
+
+                //foma inversa 
+                //if ( i % 2 != 0 ) acumuladorImpar += i ;
+                //else acumuladorPar += i ;
+                                            
+            }
+
+            Console.WriteLine("la suma de los pares es:" + acomuladorPar);
+            Console.WriteLine("la suma de los impar es:" + acomuladorImpar);
+        }
+        
+
+    }
+}
---
+++ Ejercicio14_Taller/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+
+namespace Ejercicio14_Taller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio pida 2 numeros y que muestre todos los numeros comprendidos entre ambos numeros incluyendolos 
+
+            // Declaramos las variables
+
+            double num1, num2, numMayor, numMenor;
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            // Comparamos el numero mayor y el numero menor 
+
+            if (num1 > num2)
+            {
+                numMenor = num2;
+                numMayor = num1;
+
+            }
+            else 
+            {
+                numMayor = num2;
+                numMenor = num1;
+
+
+            }
+
+            //cuenta los numeros desde el menor hasta el numero mayor de uno a uno
+            for (double i = numMenor; i <= numMayor; i++ ) 
+            {
+                Console.WriteLine(i); 
+
+            }
+            
+
+        }
+    }
+}
